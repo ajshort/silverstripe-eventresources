2a73e246dc782b95a634db4994b565de5011635c
ajshort <andrewjshort@gmail.com>
2011-02-05 11:11:01 +1100

    Added support for recurring events in the resource avaialbility checker.

--- code/dataobject/EventResource.php
+++ code/dataobject/EventResource.php
@@ -95,15 +95,21 @@ class EventResource extends DataObject {
 		$start = $time->getStartTimestamp();
 		$end   = $time->getEndTimestamp();
 
-		$filter = sprintf(
+		// First just get simple non-recurring bookings, often this will be
+		// enough to check if a resource is available.
+		$dateFilter = sprintf(
 			'"StartDate" BETWEEN \'%1$s\' AND \'%2$s\'
 			OR "EndDate" BETWEEN \'%1$s\' AND \'%2$s\'
 			OR ("StartDate" < \'%1$s\' AND "EndDate" > \'%2$s\')',
 			date('Y-m-d', $start), date('Y-m-d', $end)
 		);
-		$filter = "\"CalendarDateTimeID\" <> {$time->ID} AND ($filter)";
+		$filter = "\"CalendarEvent\".\"Recursion\" = 0 AND \"CalendarDateTimeID\" <> {$time->ID} AND ($dateFilter)";
 
-		$bookings = $this->Events($filter)->toArray('ID');
+		$bookings = $this->Events(
+			$filter,
+			null,
+			'INNER JOIN "CalendarEvent" ON "CalendarEvent"."ID" = "CalendarDateTime"."EventID"');
+		$bookings = $bookings->toArray('ID');
 
 		// Since the event calendar doesn't use a proper date time storage, we
 		// need to manually filter events again here.
@@ -113,12 +119,48 @@ class EventResource extends DataObject {
 			}
 		}
 
-		if (!count($bookings)) {
-			return $this->Type == 'Limited' ? (int) $this->Quantity : true;
+		if ($bookings && $this->Type == 'Single') {
+			return false;
 		}
 
-		if ($this->Type == 'Single') {
+		// Now also load all the recurring events, and check if they occur on
+		// this day.
+		$recurring = $this->Events(
+			sprintf(
+				'"CalendarDateTimeID" <> %d
+				AND "CalendarEvent"."Recursion" = 1
+				AND ("EndDate" IS NULL OR "EndDate" > \'%s\')
+				AND ("StartDate" IS NULL OR "StartDate" < \'%s\')',
+				$time->ID, date('Y-m-d', $start), date('Y-m-d', $end)
+			),
+			null,
+			'INNER JOIN "CalendarEvent" ON "CalendarEvent"."ID" = "CalendarDateTime"."EventID"');
+
+		// Now loop through each day this event runs on, and check if any of the
+		// events fall on it. If they do just push them onto the bookings set.
+		foreach ($recurring as $datetime) {
+			$counter = $start;
+
+			while ($counter <= $end) {
+				if ($counter > strtotime($datetime->EndDate)) {
+					break;
+				}
+
+				if ($datetime->Event()->recursionHappensOn($counter)) {
+					$bookings[$datetime->ID] = $datetime;
+					break;
+				}
+
+				$counter = sfTime::add($counter, 1, sfTime::DAY);
+			}
+		}
+
+		if ($bookings && $this->Type == 'Single') {
 			return false;
+		}
+
+		if (!count($bookings)) {
+			return $this->Type == 'Limited' ? (int) $this->Quantity : true;
 		}
 
 		$quantity = (int) $this->Quantity;
